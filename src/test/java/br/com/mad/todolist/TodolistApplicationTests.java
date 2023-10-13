602bf4a59a8329677fdd554dcdb7fbd853ebe76b
MADdantas <matheussdantas19@gmail.com>
2023-10-13 11:20:44 -0300

    first commit

---
+++ src/test/java/br/com/mad/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mad.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
